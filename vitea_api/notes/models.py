b6965ea5586a5059eacebd9b799c193c1306c205
Harshitab1005 <harshitab1004@gmail.com>
2024-04-09 22:49:57 +0530

    model for notes dne

---
+++ vitea_api/notes/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Notes(models.Model):
+    branch = [
+        ('CSE', 'CSE'),
+        ('IT','IT'),
+        ('EEE', 'EEE'),
+        ('ECE', 'ECE'),
+        ('Mechanical', 'Mechanica;'),
+        ('Civil', 'Civil'),
+        ('Law', 'Law'),
+        ('Fashion', 'Fashion'),
+        ('Business', 'Business'),
+    ]
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    content = models.FileField(upload_to='notes/')
+    branch = models.CharField(max_length=50, choices=branch, null=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+    
