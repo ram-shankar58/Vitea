946d493d79c9a61a0f674237d0ce7c98ad9d3848
Kedar <kedarshinde8011@gmail.com>
2024-04-09 22:11:14 +0530

    Comments

--- vitea_api/posts/urls.py
+++ vitea_api/posts/urls.py
@@ -3,6 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('create/', CreatePostView.as_view(), name='create_post'),
-    path('<int:pk>/get/', PostDetailView.as_view(), name='post_detail'),
-    path('<int:pk>/update/', PostDetailView.as_view(), name='post_update'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]
