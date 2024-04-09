c70ac4a756f9671720061c5fa6952c482c9c59f3
Kedar <kedarshinde8011@gmail.com>
2024-04-10 04:11:54 +0530

    Navbar

---
+++ vitea/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './Navbar.css'; // Import the CSS file
+import ProfileImage from '../assets/profile.png'; // Replace with your default profile image path
+import Logo from '../assets/vitea-logo.png';
+
+const Navbar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <nav className="nav-container">
+      <img src={Logo} alt="Your App Logo" className="styled-logo" /> {/* Replace "logo.png" with your logo path */}
+      <div className="search-bar-container">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className="search-bar"
+        />
+      </div>
+      <img src={ProfileImage} alt="Profile Picture" className="profile-icon" /> {/* Replace "profile.png" with your default profile image path */}
+      <ul className="nav-links">
+        {/* ... your navigation links ... */}
+      </ul>
+    </nav>
+
+
+  );
+};
+
+export default Navbar;
--- vitea/src/pages/HomePage.jsx
+++ vitea/src/pages/HomePage.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const HomePage = () => {
     
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <><Navbar /></>
     );
 };
 
