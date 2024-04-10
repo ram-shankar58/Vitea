d89765062697f35bfdbb9400c79464771ec079d1
Amal Shaji <61158962+AmalCodin@users.noreply.github.com>
2024-04-10 06:14:00 +0530

    Commit the message

--- vitea/src/App.jsx
+++ vitea/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/home" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   )
