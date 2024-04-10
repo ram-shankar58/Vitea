b7d635ba8f39bd7e9ff0b837ea46893934ce0b80
ram-shankar58 <sanjayramshankar@gmail.com>
2024-04-10 06:20:45 +0530

    added ad bar

--- vitea/src/pages/HomePage.jsx
+++ vitea/src/pages/HomePage.jsx
@@ -1,25 +1,30 @@
-
 import React from 'react';
 import Navbar from '../components/Navbar';
 import PopularPost from '../components/PopularPost';
 import NoticeBoard from '../components/NoticeBoard';
-
+import './HomePage.css'; // Import the CSS file
 
 const HomePage = () => {
-    
     return (
-    <>
-    <Navbar />
-
-
-    <NoticeBoard />
-    <PopularPost />
-
-    
-
-    
-
-    </>
+        <>
+            <Navbar />
+            <div className="ad-strip">
+                <div className="ad">
+                    <p>HACKATHON 1!</p>
+                    <a href="/LoginPage">Join Now!</a>
+                </div>
+                <div className="ad">
+                <p>HACKATHON 2</p>
+                <a href="/LoginPage">Join Now!</a>
+                </div>
+                <div className="ad">
+                <p>XYZ CLUB</p>
+                <a href="/LoginPage">Join Now!</a>
+                </div>
+            </div>
+            <NoticeBoard />
+            <PopularPost />
+        </>
     );
 };
 
